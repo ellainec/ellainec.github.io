17e749b37d4579c411eab6b455b0711512365645
ellainec <chan.ellaine@gmail.com>
2020-01-29 09:11:00 -0800

    lab3

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,67 @@
+function createArtistCard(bio) {
+    var div = document.createElement('div');
+    div.classList.add("artistCard");
+    var uniqueId =  Date.now();
+    div.id = uniqueId;
+    var artistBox = createArtistBox(bio);
+    var deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.addEventListener('click', function() {
+        deleteArtist(uniqueId);
+    }, false);
+    div.appendChild(artistBox);
+    div.appendChild(deleteBtn);
+    div.classList.add("lightBorder");
+    return div;
+}
+
 function createArtistBox(bio) {
     var div = document.createElement('div');
     div.classList.add("artistBox");
-    div.classList.add("lightBorder");
     var img = document.createElement('img');
     img.src = bio.img;
     var textBox = makeTextDiv(bio.name, bio.description);
+
     div.appendChild(img);
     div.appendChild(textBox);
-
-    document.getElementById('artistList').appendChild(div);
+    return div;
+    
 }
 
 function makeTextDiv(name, desc) {
     var div = document.createElement('div');
     div.classList.add("artistBio");
-    var header = document.createElement('h2');
-    header.innerHTML = name;
+    var header = document.createElement('h3');
+    header.innerText = name;
     header.classList.add("artistName");
     var description = document.createElement('span');
-    description.innerHTML = desc;
+    description.innerText = desc;
     description.classList.add("artistDescription");
     div.appendChild(header);
     div.appendChild(description);
     return div;
 }
 
-var bios = [ 
-    {name: "Barot Bellingham",
-     description: "Royal Academy of Painting and Sculpture",
-     img: "https://randomuser.me/api/portraits/med/men/1.jpg"}, 
-     {name: "Jonathan G.Ferrar II",
-     description: "Artist to Watch in 2012",
-     img: "https://randomuser.me/api/portraits/med/men/2.jpg"}, 
-     {name: "Hillary Hewit Goldwynn",
-     description: "New York University",
-     img: "https://randomuser.me/api/portraits/med/women/1.jpg"}, 
-     {name: "Hassum Harrod",
-     description: "Art College in New Dehli",
-     img: "https://randomuser.me/api/portraits/med/men/3.jpg"}, 
-     {name: "Jennifer Jerome",
-     description: "Master Baker",
-     img: "https://randomuser.me/api/portraits/med/women/2.jpg"}, 
-]
-
-
-for(let i = 0; i < 5; i++) {
-    createArtistBox(bios[i]);
+function toggleForm() {
+    var form = document.getElementById('addForm');
+    form.classList.toggle('hide');
 }
+
+function addArtist() {
+    console.log("hello");
+    var name = document.getElementById('nameInput').value;
+    var description = document.getElementById('aboutInput').value;
+    var img = document.getElementById('imgInput').value;
+    var artistCard = createArtistCard({name, description, img});
+    document.getElementById('artistList').appendChild(artistCard);
+}
+
+function deleteArtist(id) {
+    var artist = document.getElementById(id);
+    document.getElementById('artistList').removeChild(artist);
+}
+
+document.getElementById('addBtn').addEventListener('click', function() {
+    addArtist();
+})
